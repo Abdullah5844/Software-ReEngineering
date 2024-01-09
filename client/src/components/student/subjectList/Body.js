757b11017ec82731a08f3b53e06d03467b23b606
Kazim786-dev <kazimasif2020@gmail.com>
2024-01-09 22:12:29 +0500

    use of UseEffect and removing unused

--- client/src/components/student/subjectList/Body.js
+++ client/src/components/student/subjectList/Body.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import { useDispatch, useSelector } from "react-redux";
-import { getSubject } from "../../../redux/actions/adminActions";
+// import { getSubject } from "../../../redux/actions/adminActions";
 // import { MenuItem, Select } from "@mui/material";
 import Spinner from "../../../utils/Spinner";
 import { SET_ERRORS } from "../../../redux/actionTypes";
@@ -12,11 +12,11 @@ const Body = () => {
   const [error, setError] = useState({});
   const [loading, setLoading] = useState(false);
   const store = useSelector((state) => state);
-  const [value, setValue] = useState({
-    department: "",
-    year: "",
-  });
-  const [search, setSearch] = useState(false);
+  // const [value, setValue] = useState({
+  //   department: "",
+  //   year: "",
+  // });
+  // const [search, setSearch] = useState(false);
 
   useEffect(() => {
     if (Object.keys(store.errors).length !== 0) {
@@ -25,13 +25,13 @@ const Body = () => {
     }
   }, [store.errors]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSearch(true);
-    setLoading(true);
-    setError({});
-    dispatch(getSubject(value));
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setSearch(true);
+  //   setLoading(true);
+  //   setError({});
+  //   dispatch(getSubject(value));
+  // };
   const subjects = useSelector((state) => state.admin.subjects.result);
 
   useEffect(() => {
